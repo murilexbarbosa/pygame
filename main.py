0261dcd44c74c5af6808c4a929941254d198fe1e
unknown <muka040789@hotmail.com>
2020-10-09 19:13:19 -0300

    add backgroud and song to the game

--- main.py
+++ main.py
@@ -3,16 +3,23 @@
 
 # Our Games Imports
 import pygame
+from pygame import mixer
 
 pygame.init()
+mixer.init()
 screen = pygame.display.set_mode((500, 500))
 done = False
 x=60
 y=60
+image=pygame.image.load(r'wallpaper.jpg')
+screen.blit(image, (0, 0))
+pygame.mixer.music.load(r'song.mp3')
+pygame.mixer.music.play(-1)
 
 while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-        pygame.draw.rect(screen, (0,128,255), pygame.Rect(x, y, 90, 90))        
+        pygame.draw.rect(screen, (255,0,0), pygame.Rect(x, y, 90, 90))
+        
         pygame.display.flip()
