93bfb0e3bc80d764aba417145ca17fd05a6061dd
unknown <muka040789@hotmail.com>
2020-10-09 19:16:59 -0300

    Changing the color of the box

--- main.py
+++ main.py
@@ -17,9 +17,17 @@ pygame.mixer.music.load(r'song.mp3')
 pygame.mixer.music.play(-1)
 
 while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        pygame.draw.rect(screen, (255,0,0), pygame.Rect(x, y, 90, 90))
-        
-        pygame.display.flip()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+	pygame.draw.rect(screen, (255,0,0), pygame.Rect(x, y, 90, 90))
+	
+	pygame.display.flip()
+	
+	is_red = True
+	#in infinite while loop:-
+	if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+							is_red = not is_red
+			if is_red: color = (255, 0, 0)
+			else: color = (102, 0, 0)			
+	
